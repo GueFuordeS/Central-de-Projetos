6010af7f70d6213ef4bcf44f1a3b1aa0ee0eb5dc
gabrielfernandesufcg <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2017-03-11 00:25:59 -0300

    fase a

---
+++ Central de Projetos/src/excecoes/ValidacaoException.java
@@ -0,0 +1,15 @@
+package excecoes;
+
+public class ValidacaoException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ValidacaoException() {
+		super("Erro na validacao");
+	}
+	
+	public ValidacaoException(String mensagem) {
+		super(mensagem);
+	}
+
+}
--- Central de Projetos/src/pessoa/Pessoa.java
+++ Central de Projetos/src/pessoa/Pessoa.java
@@ -1,27 +1,55 @@
 package pessoa;
 
+import excecoes.Validacao;
+import excecoes.ValidacaoException;
+
 public class Pessoa {
 	private String cpf;
 	private String nome;
 	private String email;
 	
-	public Pessoa(String cpf, String nome, String email) throws Exception {
-		if(cpf == null || cpf.trim().isEmpty()) {
-			throw new Exception("Erro no cadastro de pessoa. CPF nulo ou vazio");
-		}
-		
-		if(nome == null || nome.trim().isEmpty()) {
-			throw new Exception("Erro no cadastro de pessoa. ");
-		}
-		
-		if(email == null || email.trim().isEmpty()) {
-			throw new Exception("Erro no cadastro de pessoa.");
-		}
+	public Pessoa(String cpf, String nome, String email) throws ValidacaoException {
+		Validacao.validaCpf(cpf);
+		Validacao.validaString(nome);
+		Validacao.validaString(email);
 		
 		this.cpf = cpf;
 		this.nome = nome;
 		this.email = email;
 	}
 	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String getCpf() {
+		return this.cpf;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public Boolean setNome(String nome) {
+		try {
+			Validacao.validaString(nome);
+			this.nome = nome;
+			return true;
+		}
+		catch (ValidacaoException e) {
+			return false;
+		}
+	}
+	
+	public Boolean setEmail(String email) {
+		try {
+			Validacao.validaString(email);
+			this.nome = email;
+			return true;
+		}
+		catch (ValidacaoException e) {
+			return false;
+		}
+	}
 	
 }
